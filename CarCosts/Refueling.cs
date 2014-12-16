54c1b2eb3a35f02945c43142d476f42f8010307e
Christoph Bergmann <c.bergmann2@gmx.net>
2014-12-16 20:11:40 +0100

    Add class Refueling

---
+++ CarCosts/Refueling.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCosts
+{
+    class Refueling
+    {
+        public DateTime date;           //Date of refueling action
+        public double amount;           //Refueling amount
+        public bool isCompleteFilled;   //true if tank was complete filled
+        public double costs;            //total costs of refueling
+        public double drivenDistance;   //driven distance since last refueling
+
+    }
+}
